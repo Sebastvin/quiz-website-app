6048b3199e80fcef80057ec9a26efe2dddf3ae5e
unknown <Sebastvin@gmail.com>
2022-05-13 15:34:43 +0200

    json fromat change script

--- converter.py
+++ converter.py
@@ -4,7 +4,33 @@ f = open('data.json')
 
 data = json.load(f)
 
-for x in data:
-    print(x['question'].replace("&quot;", "\"").replace("&#039;", "`"))
-    print(x['correct_answer'])
+# for x in data:
+#     print(x['question'].replace("&quot;", "\"").replace("&#039;", "`"))
+#     print(x['correct_answer'].replace("&quot;", "\"").replace("&#039;", "`"))
+#     x['incorrect_answers'] = [x.replace("&quot;", "\"").replace("&#039;", "`") for x in x['incorrect_answers']]
+#     print(x['incorrect_answers'])
+
+# print(data)
+
+def randomize_questions_and_answers(questions, answer):
+
+
+for x in data[:1]:
+    x['incorrect_answers'].append(x['correct_answer'])
+
     print(x['incorrect_answers'])
+    print(x['correct_answer'])
+    picks = x['incorrect_answers']
+
+    tmp = {
+        "question": x['question'],
+        "A": picks[0],
+        "B": picks[1],
+        "C": picks[2],
+        "D": picks[3],
+        "answer": x['correct_answer']
+    }
+
+
+
+print(tmp)
