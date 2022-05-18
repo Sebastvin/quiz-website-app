47c07dcd1570a67bc070f432d91399a4eb8946f8
unknown <Sebastvin@gmail.com>
2022-05-18 14:39:21 +0200

    requirements.txt

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ app = Flask(__name__)
 def main_page():
     return render_template("main.html")
 
-#
-# @app.route("/quiz", methods=['POST'])
-# def quiz():
-#     category = request.form["quiz_button"]
-#     print(category)
-#     data = list(db_functions.format_questions(db_functions.generate_questions(10, category)))
-#     if request.method == 'POST':
-#         return render_template("questions.html", data=data)
+
+@app.route("/quiz", methods=['POST'])
+def quiz():
+    category = request.form["quiz_button"]
+    # print(category)
+    # data = list(db_functions.format_questions(db_functions.generate_questions(10, category)))
+    if request.method == 'POST':
+        return render_template("questions.html", data=category)
 
 
 if __name__ == "__main__":
